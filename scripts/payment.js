d1149c97d516edb7d9aeac0b6d1ac2455c2f93a7
dillonago <71573711+dillonago@users.noreply.github.com>
2021-10-01 13:16:14 -0700

    Add files via upload

--- scripts/payment.js
+++ scripts/payment.js
@@ -2,13 +2,19 @@
     'use strict';
     var FORM_SELECTOR = '[payment-info="form"]';
     var FormHandler = App.FormHandler;
-    var formHandler = new FormHandler(FORM_SELECTOR);
+    var payFormHandler = new FormHandler(FORM_SELECTOR);
 
-    formHandler.addSubmitHandler(function (data) {
+    payFormHandler.addSubmitHandler(function (data) {
         var str = "Thank you for your payment, " + data.title + " " + data.username;
         $('#thanks-modal-body').html(str);
         $('#thanks-modal').modal('show');
+        $('#myModal').on('hidden.bs.modal', function () {
+            window.location.pathname=('/');
+            console.log(window.location);
+          })
+        
         console.log("Submitted");
+        
     });
-    console.log(formHandler);
+    console.log(payFormHandler);
 })(window);
